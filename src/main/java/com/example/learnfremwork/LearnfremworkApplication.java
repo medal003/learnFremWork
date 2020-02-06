7ee1420cec2810e97c753d8dff701667dc8b1dfa
meideng.zh <medal003@126.com>
2020-02-06 11:08:46 +0800

    Initial commit

---
+++ src/main/java/com/example/learnfremwork/LearnfremworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnfremwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnfremworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnfremworkApplication.class, args);
+    }
+
+}
