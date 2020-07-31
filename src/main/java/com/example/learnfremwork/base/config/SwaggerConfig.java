bc4f04ad3f41f23d34270c7328a49c9c966602c7
meideng.zh <medal003@126.com>
2020-07-31 16:41:17 +0800

    添加knife4j的配置

---
+++ src/main/java/com/example/learnfremwork/base/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.example.learnfremwork.base.config;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author meideng.zh <br/>
+ * date: 2020/7/11/0011 17:09:47 <br/>
+ * comment: 
+ */
+@Configuration
+@EnableSwagger2
+@EnableKnife4j
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(true)
+                .apiInfo(apiInfo())
+                //分组名称
+                .groupName("2.X版本")
+                .select()
+                //这里指定Controller扫描包路径
+                .apis(RequestHandlerSelectors.basePackage("com.example.learnfremwork.controll"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("简单课程表-API文档")
+                .description("本文档描述了简单课程表系统的接口定义")
+                .version("1.0")
+                .build();
+    }
+}
